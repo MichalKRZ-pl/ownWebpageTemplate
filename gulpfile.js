c82a5e9c0d515620f7cf40f3e6354d88c9e6d49a
Michal Krz <michal.krzywdzinski@outlook.com>
2021-12-05 12:21:13 +0100

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,98 @@
+const {src,dest, series, parallel, watch} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const cssnano = require('gulp-cssnano');
+const autoprefixer = require('gulp-autoprefixer');
+const rename = require("gulp-rename");
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync').create();
+const clean = require('gulp-clean');
+const kit = require('gulp-kit');
+
+
+const paths ={
+    sass: './src/sass/**/*.scss',
+    jScript: './src/js/**/*.js',
+    images: './src/img/*',
+    dist: './dist',
+    html: './html/**/*.kit',
+    sassDest: './dist/css',
+    jScriptDest: './dist/js',
+    imgDest: './dist/img'
+}
+// sass.compiler = require('node-sass')
+
+function sassCompiler(done) {
+    src(paths.sass)
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(cssnano())
+    .pipe(rename({
+        suffix: ".min"
+      }))
+    .pipe(sourcemaps.write())
+    .pipe(dest(paths.sassDest));
+    done();
+}
+
+function babelScript(done){
+    src(paths.jScript)
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+       presets: ['@babel/env']
+    }))
+    .pipe(uglify())
+    .pipe(rename({
+        suffix: ".min"
+      }))
+    .pipe(sourcemaps.write())
+    .pipe(dest(paths.jScriptDest));
+    done()
+}
+
+function imageShrink(done){
+    src(paths.images)
+    .pipe(imagemin())
+    .pipe(rename({
+        suffix: ".min"
+      }))
+    .pipe(dest(paths.imgDest));
+    done()
+}
+
+function handleKits(done){
+    src(paths.html)
+    .pipe(kit())
+    .pipe(dest('./'))
+    done()
+}
+
+function startBrowserSync(done){
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    })
+    done()
+}
+
+function watchForChanges(done){
+    watch('./*.html').on("change", browserSync.reload);
+    watch([paths.html ,paths.sass, paths.jScript], parallel(handleKits, babelScript, sassCompiler)).on("change",browserSync.reload);
+    watch(paths.images,imageShrink).on("change", browserSync.reload);
+    done()
+}
+
+function cleanStuff(done){
+    src(paths.dist, {read: false})
+    .pipe(clean());
+    done()
+}
+
+const mainFunctions = parallel(handleKits ,babelScript, sassCompiler, imageShrink)
+exports.default =series(mainFunctions, startBrowserSync, watchForChanges)
+
+exports.cleanStuff = cleanStuff
