b2428dbae5640f91fea6aafade494c7a52c59d71
Michal Krz <michal.krzywdzinski@outlook.com>
2022-01-21 19:27:05 +0100

    added script to keep footer year updated

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 let $burgerBtn;
 let $mobileMenu;
+let $footerYear;
 
 const main = () => {
   prepareDOMElements();
@@ -9,10 +10,17 @@ const main = () => {
 const prepareDOMElements = () => {
   $burgerBtn = document.querySelector(".header__burger");
   $mobileMenu = document.querySelector(".mobile");
+  $footerYear = document.querySelector(".footer__year");
 };
 
 const prepareDOMEvents = () => {
   $burgerBtn.addEventListener("click", handleBurger);
+  handleFooterYear();
+};
+
+const handleFooterYear = () => {
+  const year = new Date().getFullYear();
+  $footerYear.innerText = year;
 };
 
 const handleBurger = () => {
