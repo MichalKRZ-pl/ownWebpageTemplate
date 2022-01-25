1fc4fae14b8fb249747a1035903a4e489f29c346
Michal Krz <michal.krzywdzinski@outlook.com>
2022-01-25 20:06:49 +0100

    slick media styles and custom buttons for slick controls

--- src/js/slicksettings.js
+++ src/js/slicksettings.js
@@ -1,6 +1,34 @@
 $(".services__carousel-track").slick({
-  slidesToShow: 3,
+  slidesToShow: 4,
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 2000,
+  mobilefirst: true,
+  autoplay: true,
+  arrows: false,
+
+  responsive: [
+    {
+      breakpoint: 576,
+      settings: { slidesToShow: 1 },
+    },
+
+    {
+      breakpoint: 768,
+      settings: { slidesToShow: 2 },
+    },
+
+    {
+      breakpoint: 1200,
+      settings: { slidesToShow: 3 },
+    },
+  ],
+});
+
+$(".services__carousel-btn--left").click(function () {
+  $(".services__carousel-track").slick("slickPrev");
+});
+
+$(".services__carousel-btn--right").click(function () {
+  $(".services__carousel-track").slick("slickNext");
 });
